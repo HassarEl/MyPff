92c90ae21f088f306cba2af5184f0e1253c13192
EL Mehdi HASSAR <mido.hass.31@gmail.com>
2023-04-23 15:05:12 +0100

    add jardinier route in auth middelware

--- routes/web.php
+++ routes/web.php
@@ -23,21 +23,24 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route Jardin 
+    Route::get('/jardin', [JardinController::class, 'index'])->name('jardin');
+    Route::get('/jardin/create', [JardinController::class, 'create'])->name('jardin.create');
+    Route::post('/jardin/store', [JardinController::class, 'store'])->name('jardin.store');
+    Route::get('/jardin/{id}', [JardinController::class, 'show'])->name('jardin.show');
 });
 
 require __DIR__.'/auth.php';
 
-// Route Jardin 
-Route::get('/jardin', [JardinController::class, 'index'])->name('jardin');
-Route::get('/jardin/create', [JardinController::class, 'create'])->name('jardin.create');
-Route::post('/jardin/store', [JardinController::class, 'store'])->name('jardin.store');
-Route::get('/jardin/{id}', [JardinController::class, 'show'])->name('jardin.show');
+
 
 // Route Jardinier
 Route::get('/jardinier',function(){
-    return view('jardinier.index');
+    $jardinier = User::all();
+    return view('jardinier.index', compact('jardinier'));
 })->name('jardinier');
